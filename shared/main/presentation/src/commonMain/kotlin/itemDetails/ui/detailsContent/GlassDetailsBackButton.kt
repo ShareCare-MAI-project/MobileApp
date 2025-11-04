35d3122672ba58a863230a928528a26554e30e30
Artyom Matashkov <aimatashkov@mai.education>
2025-11-04 20:14:24 +0300

    - Update cmp & md3 version - Add BackGlassButton

--- shared/main/presentation/src/commonMain/kotlin/itemDetails/ui/detailsContent/GlassDetailsBackButton.kt
+++ shared/main/presentation/src/commonMain/kotlin/itemDetails/ui/detailsContent/GlassDetailsBackButton.kt
@@ -7,22 +7,10 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.rounded.ArrowBackIos
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import dev.chrisbanes.haze.HazeState
-import view.consts.Paddings
-import widgets.glass.GlassCard
-import widgets.glass.GlassCardFunctions
+import widgets.glass.BackGlassButton
 
 @Composable
 internal fun GlassDetailsBackButton(
@@ -43,31 +31,9 @@ internal fun GlassDetailsBackButton(
         ),
         exit = fadeOut() + scaleOut()
     ) {
-        GlassCard(
-            // Because of Padding...(about clip)
-            modifier = Modifier
-                .clip(CircleShape)
-                .clickable {
-                    onClick()
-                },
+        BackGlassButton(
             hazeState = hazeState,
-            shape = CircleShape,
-            contentPadding = PaddingValues(Paddings.semiMedium),
-            contentAlignment = Alignment.Center,
-            tint = Color.Transparent,
-            hazeTint = GlassCardFunctions.getHazeTintColor(
-                tint = null,
-                containerColor = colorScheme.primaryContainer,
-                containerColorAlpha = .4f
-            ),
-            borderColor = GlassCardFunctions.getBorderColor(
-                tint = null,
-                containerColor = colorScheme.primaryContainer,
-                containerColorAlpha = .1f
-            ),
-            contentColor = Color.White
-        ) {
-            Icon(Icons.AutoMirrored.Rounded.ArrowBackIos, contentDescription = null)
-        }
+            onClick = onClick
+        )
     }
 }
