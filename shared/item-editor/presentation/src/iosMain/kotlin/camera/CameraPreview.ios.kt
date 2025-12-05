b65524c7505f7a068211fbfb1ccdd433cf8e63df
Artyom Matashkov <aimatashkov@mai.education>
2025-12-05 13:41:47 +0300

    Centrize CameraEmulatorError

--- shared/item-editor/presentation/src/iosMain/kotlin/camera/CameraPreview.ios.kt
+++ shared/item-editor/presentation/src/iosMain/kotlin/camera/CameraPreview.ios.kt
@@ -1,9 +1,12 @@
 package camera
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.viewinterop.UIKitView
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.readValue
@@ -66,9 +69,14 @@ actual fun CameraPreview(
             }
         )
     } else {
-        Text( """
+        Box(modifier, contentAlignment = Alignment.Center) {
+            Text(
+                """
             Camera is not available on simulator.
             Please try to run on a real iOS device.
-        """)
+        """,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
