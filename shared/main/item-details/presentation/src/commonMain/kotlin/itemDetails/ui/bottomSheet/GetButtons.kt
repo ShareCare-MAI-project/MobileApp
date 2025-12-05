8a421fa4758b2a5e97227ccdb105949a73e55dd8
Artyom Matashkov <aimatashkov@mai.education>
2025-12-05 12:03:33 +0300

    Ask before deleting

--- shared/main/item-details/presentation/src/commonMain/kotlin/itemDetails/ui/bottomSheet/GetButtons.kt
+++ shared/main/item-details/presentation/src/commonMain/kotlin/itemDetails/ui/bottomSheet/GetButtons.kt
@@ -12,7 +12,10 @@ import androidx.compose.material.icons.rounded.Done
 import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import itemDetails.components.ItemDetailsComponent
 import view.theme.colors.CustomColors
 import widgets.expressiveList.ExpressiveListItem
@@ -24,80 +27,104 @@ fun rememberButtons(
     component: ItemDetailsComponent,
     closeSheet: (() -> Unit) -> Unit
 ): List<ExpressiveListItem> {
+
+    var isDeleting by remember { mutableStateOf(false) }
+
+
     val containerColor = colorScheme.surfaceContainerHighest.copy(alpha = .6f)
     val primaryColor = colorScheme.primary
     val errorColor = colorScheme.error
     val greenColor = if (isSystemInDarkTheme()) CustomColors.green else CustomColors.darkGreen
-    return remember(recipientId) {
+
+
+    val deleteItem = ExpressiveListItem(
+        icon = Icons.Rounded.DeleteOutline,
+        text = "Удалить",
+        containerColor = containerColor,
+        contentColor = errorColor
+    ) {
+        if (isDeleting) {
+            component.deleteItem(closeSheet)
+        } else {
+            isDeleting = true
+        }
+    }
+
+
+    return remember(recipientId, isDeleting) {
         buildList<ExpressiveListItem> {
-            if (isOwner) {
-                if (recipientId == null) {
-                    add(
-                        ExpressiveListItem(
-                            icon = Icons.Rounded.DeleteOutline,
-                            text = "Удалить",
-                            containerColor = containerColor,
-                            contentColor = errorColor
-                        ) {
-                            component.deleteItem(closeSheet)
-                        }
-                    )
-                    add(
-                        ExpressiveListItem(
-                            icon = Icons.Rounded.Edit,
-                            text = "Редактировать",
-                            containerColor = containerColor,
-                            contentColor = primaryColor
-                        ) {
-                            closeSheet {}
-                            component.onEditClick()
-                        }
-                    )
-                }
-            }
-            if (recipientId != null) {
+            if (isDeleting) {
+                add(deleteItem)
                 add(
                     ExpressiveListItem(
                         icon = Icons.Rounded.Close,
-                        text = if (isOwner) "Отклонить" else "Отменить",
-                        containerColor = containerColor,
-                        contentColor = errorColor
-                    ) {
-                        component.denyItem()
-                    }
+                        text = "Отмена",
+                        onClick = { isDeleting = false }
+                    )
                 )
-                add(
-                    ExpressiveListItem(
-                        icon = Icons.Rounded.Done,
-                        text = if (isOwner) "Отдал" else "Получил",
-                        containerColor = containerColor,
-                        contentColor = greenColor
-                    ) { component.acceptItem(closeSheet) }
-                )
-                add(
-                    ExpressiveListItem(
-                        icon = Icons.AutoMirrored.Rounded.Send,
-                        text = "Переписка",
-                        containerColor = containerColor,
-                        contentColor = primaryColor
-                    ) {
-                        AlertsManager.push(
-                            AlertState.SnackBar(
-                                component.telegram.value ?: "Неизвестный телеграм"
-                            )
+            } else {
+                if (isOwner) {
+                    if (recipientId == null) {
+                        add(
+                            deleteItem
+                        )
+                        add(
+                            ExpressiveListItem(
+                                icon = Icons.Rounded.Edit,
+                                text = "Редактировать",
+                                containerColor = containerColor,
+                                contentColor = primaryColor
+                            ) {
+                                closeSheet {}
+                                component.onEditClick()
+                            }
                         )
                     }
-                )
-            } else if (!isOwner) {
-                add(
-                    ExpressiveListItem(
-                        icon = Icons.Rounded.AddBox,
-                        text = "Забрать вещь",
-                        containerColor = containerColor,
-                        contentColor = greenColor,
-                        blendy = .15f
-                    ) { component.takeItem() }
-                )
+                }
+                if (recipientId != null) {
+                    add(
+                        ExpressiveListItem(
+                            icon = Icons.Rounded.Close,
+                            text = if (isOwner) "Отклонить" else "Отменить",
+                            containerColor = containerColor,
+                            contentColor = errorColor
+                        ) {
+                            component.denyItem()
+                        }
+                    )
+                    add(
+                        ExpressiveListItem(
+                            icon = Icons.Rounded.Done,
+                            text = if (isOwner) "Отдал" else "Получил",
+                            containerColor = containerColor,
+                            contentColor = greenColor
+                        ) { component.acceptItem(closeSheet) }
+                    )
+                    add(
+                        ExpressiveListItem(
+                            icon = Icons.AutoMirrored.Rounded.Send,
+                            text = "Переписка",
+                            containerColor = containerColor,
+                            contentColor = primaryColor
+                        ) {
+                            AlertsManager.push(
+                                AlertState.SnackBar(
+                                    component.telegram.value ?: "Неизвестный телеграм"
+                                )
+                            )
+                        }
+                    )
+                } else if (!isOwner) {
+                    add(
+                        ExpressiveListItem(
+                            icon = Icons.Rounded.AddBox,
+                            text = "Забрать вещь",
+                            containerColor = containerColor,
+                            contentColor = greenColor,
+                            blendy = .15f
+                        ) { component.takeItem() }
+                    )
+                }
             }
         }
     }
